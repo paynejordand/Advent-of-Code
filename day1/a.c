53d43416969291dbddf102c0315a474959f448e9
Jordan <paynejordand@gmail.com>
2021-12-01 11:23:34 -0500

    fixed a

--- day1/a.c
+++ day1/a.c
@@ -6,17 +6,13 @@ int main()
     int currNum;
     int increased = 0;
 
-    char line[5];
-
     FILE* file = fopen("input.txt", "r");
 
-    fgets(line, 5, file);
-    sscanf(line, "%d", &lastNum);
+    fscanf(file, "%d ", &lastNum);
 
     while(!feof(file))
     {
-        fgets(line, 5, file);
-        sscanf(line, "%d", &currNum);
+        fscanf(file, "%d ", &currNum);
         if (currNum > lastNum)
         {
             increased++;
