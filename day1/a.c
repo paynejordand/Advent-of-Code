a266d2b3779b5a7deffa0c9e1ba11bca56d445a6
Jordan <paynejordand@gmail.com>
2021-12-01 11:51:46 -0500

    cleaned up a lil

--- day1/a.c
+++ day1/a.c
@@ -2,25 +2,20 @@
 
 int main()
 {
-    int lastNum;
+    int lastNum = 99999999;
     int currNum;
     int increased = 0;
-
     FILE* file = fopen("input.txt", "r");
-
-    fscanf(file, "%d ", &lastNum);
-
+    
     while(!feof(file))
     {
         fscanf(file, "%d ", &currNum);
         if (currNum > lastNum)
-        {
             increased++;
-        }
+        
         lastNum = currNum;
     }
-
     printf("%d\n", increased);
-
+    fclose(file);
     return 0;
 }
--- day1/b.c
+++ day1/b.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
-
 int main()
 {
-    int nums[3];
-    int curr_sum;
-    int last_sum;
+    int nums[3], curr_sum;
+    int last_sum = 0;
     int increased = 0;
     int replace = 0;
 
@@ -14,23 +11,21 @@ int main()
     for(int i = 0; i < 3; i++)
     {
         fscanf(file, " %d ", &nums[i]);
+        last_sum += nums[i];
     }
-    last_sum = nums[0] + nums[1] + nums[2];
     
     while(!feof(file))
     {
         fscanf(file, " %d ", &nums[replace]);
-
         curr_sum = nums[0] + nums[1] + nums[2];
         if(curr_sum > last_sum)
-        {
             increased++;
-        }
+        
         last_sum = curr_sum;
         replace = (replace + 1) % 3;
     }
-
     printf("%d \n", increased);
+    fclose(file);
 
     return 0;
 }
