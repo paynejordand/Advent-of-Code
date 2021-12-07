73e17acc5f5e2e02ac5f1cf6e733ea8392928115
Jordan <paynejordand@gmail.com>
2021-12-07 09:01:45 -0500

    done

--- day7/b.c
+++ day7/b.c
@@ -4,24 +4,24 @@
 int main(void)
 {
     int pos[1000];
-    int count = 0;    // input:1000 test:10
-    int max = 1937;      // input:1937 //test:16
-    int min = 0;
+    int count = 0;
+    int max = 0;
     while(!feof(stdin))
     {   
-        scanf("%d,", &pos[count++]);
+        scanf("%d,", &pos[count]);
+        max = pos[count] > max ? pos[count] : max;
+        count++;
     }
 
 
     int minCost = 1410065407;
-    for(int i = min; i <= max; i++)
+    for(int i = 0; i <= max; i++)
     {
         int cost = 0;
         for(int j = 0; j < count; j++)
         {
             int steps = (abs(pos[j] - i));
-            for(int j = 1; j <= steps; j++)
-                cost += j;
+            cost += (steps*(steps+1)/2);
         }
         minCost = cost < minCost ? cost : minCost;
     }
